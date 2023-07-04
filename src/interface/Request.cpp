b4f4e9d162b6ffbfe06ad89a9cab60ac97c75f09
MatteoTamine1709 <teamados@gmail.com>
2023-07-04 14:20:32 +0200

    TODO for json

--- src/interface/Request.cpp
+++ src/interface/Request.cpp
@@ -43,12 +43,12 @@ Request::Request(const HttpRequestHeader &header, const TcpServer &server)
     xhr = m_headers.find("X-Requested-With") != m_headers.end() &&
           m_headers["X-Requested-With"] == "XMLHttpRequest";
     body = {{"blob", header.getBody()}};
-    if (m_headers.find("Content-Type") != m_headers.end() &&
-        m_headers["Content-Type"].find("application/json") !=
-            std::string::npos) {
-        if (!header.getBody().empty())
-            body = nlohmann::json::parse(header.getBody());
-    }
+    // if (m_headers.find("Content-Type") != m_headers.end() &&
+    //     m_headers["Content-Type"].find("application/json") !=
+    //         std::string::npos) {
+    //     if (!header.getBody().empty())
+    //         body = nlohmann::json::parse(header.getBody());
+    // } TODO: Move that in a middleware
 }
 
 std::optional<std::string> Request::accepts(
